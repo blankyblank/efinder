23feb37bf953f7d12e25435da9cd027c1b2ca5f4
blankyblank <jwthaparc@gmail.com>
2025-09-18 03:49:06 -0500

    changed some message formatting in install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=2016
 set -e
 
-_install_msg_home='Installed efinder in $HOME/.local/bin
-if you dont have .local/bin in your path already put this in your shell configuration file
+_install_msg_home="Installed efinder in $HOME/.local/bin"
+_pathmgs='if you dont have .local/bin in your path already put this in your shell configuration file
 PATH="$PATH":"$HOME"/.local/bin'
 
 
@@ -13,14 +13,16 @@ install() {
   if [ "$(id -u)" != 0 ]; then 
     if [[ -d $HOME/.local/bin ]]; then 
       cp -v ./efinder "$HOME"/.local/bin/ 
-      printf '%s\n' "$_install_msg_home"
+      printf '\n%s\n\n' "$_install_msg_home"
+      printf '%s\n\n' "$_pathmgs"
     else
       mkdir -vp "$HOME"/.local/bin && cp -v ./efinder "$HOME"/.local/bin/
-      printf '%s\n' "$_install_msg_home"
+      printf '\n%s\n\n' "$_install_msg_home"
+      printf '%s\n\n' "$_pathmgs"
     fi
   else
     cp -v ./efinder /usr/local/bin/
-    printf '%s\n' ' Installed efinder system wide'
+    printf '\n%s\n\n' ' Installed efinder system wide'
   fi
   exit
 }
@@ -29,7 +31,7 @@ uninstall() {
   if [ "$(id -u)" != 0 ]; then 
     if [[ -e $HOME/.local/bin/efinder ]]; then
       rm -v "$HOME"/.local/bin/efinder
-      printf '%s\n' "Uninstalled efinder"
+      printf '\n%s\n\n' "Uninstalled efinder"
       exit
     else
       printf '%s\n' "efinder not found in $HOME/.local/bin"
@@ -38,7 +40,7 @@ uninstall() {
   else
     if [[ -e /usr/local/bin/efinder ]]; then
       rm -v /usr/local/bin/efinder
-      printf '%s\n' "Uninstalled efinder system wide"
+      printf '\n%s\n\n' "Uninstalled efinder system wide"
       exit
     else
       printf '%s\n' "efinder not found in /usr/local/bin"
